3f8fb48f8057e642bfebd140229e67fbd0584d28
jinhyeon-dev <kjh101807@gmail.com>
2023-12-12 23:04:53 +0900

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,361 @@
+function page3btn() {
+    let btn1 = document.querySelector('.page3-container');
+    let btn2 = document.querySelector('.page3-btn');
+
+    if (btn1.style.display == 'none') {
+        btn1.style.display = 'block';
+        btn2.style.display = 'none';
+    } else {
+        btn1.style.display = 'none';
+        btn2.style.display = 'flex';
+    }
+}
+
+let sum = 0;
+
+//one
+function clickOneOk(a, b) {
+    a = document.querySelector('.ok-one');
+    b = document.querySelector('.no-one');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clickOneNo(a, b) {
+    a = document.querySelector('.ok-one');
+    b = document.querySelector('.no-one');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+//two
+function clicktwoOk(a, b) {
+    a = document.querySelector('.ok-two');
+    b = document.querySelector('.no-two');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clicktwoNo(a, b) {
+    a = document.querySelector('.ok-two');
+    b = document.querySelector('.no-two');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+// three
+function clickthreeOk(a, b) {
+    a = document.querySelector('.ok-three');
+    b = document.querySelector('.no-three');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clickthreeNo(a, b) {
+    a = document.querySelector('.ok-three');
+    b = document.querySelector('.no-three');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+//four
+function clickfourOk(a, b) {
+    a = document.querySelector('.ok-four');
+    b = document.querySelector('.no-four');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clickfourNo(a, b) {
+    a = document.querySelector('.ok-four');
+    b = document.querySelector('.no-four');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+//five
+function clickfiveOk(a, b) {
+    a = document.querySelector('.ok-five');
+    b = document.querySelector('.no-five');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clickfiveNo(a, b) {
+    a = document.querySelector('.ok-five');
+    b = document.querySelector('.no-five');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+//six
+function clicksixOk(a, b) {
+    a = document.querySelector('.ok-six');
+    b = document.querySelector('.no-six');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clicksixNo(a, b) {
+    a = document.querySelector('.ok-six');
+    b = document.querySelector('.no-six');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+//seven
+function clicksevenOk(a, b) {
+    a = document.querySelector('.ok-seven');
+    b = document.querySelector('.no-seven');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clicksevenNo(a, b) {
+    a = document.querySelector('.ok-seven');
+    b = document.querySelector('.no-seven');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+//eight
+function clickeightOk(a, b) {
+    a = document.querySelector('.ok-eight');
+    b = document.querySelector('.no-eight');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clickeightNo(a, b) {
+    a = document.querySelector('.ok-eight');
+    b = document.querySelector('.no-eight');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+//nine
+function clicknineOk(a, b) {
+    a = document.querySelector('.ok-nine');
+    b = document.querySelector('.no-nine');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clicknineNo(a, b) {
+    a = document.querySelector('.ok-nine');
+    b = document.querySelector('.no-nine');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+
+//ten
+function clicktenOk(a, b) {
+    a = document.querySelector('.ok-ten');
+    b = document.querySelector('.no-ten');
+    if (a.style.backgroundColor = 'skyblue') {
+        a.style.backgroundColor = 'lightgreen';
+        b.style.backgroundColor = 'skyblue';
+    } else {
+        a.style.backgroundColor = 'skyblue';
+        b.style.backgroundColor = 'lightgreen';
+    }
+}
+
+function clicktenNo(a, b) {
+    a = document.querySelector('.ok-ten');
+    b = document.querySelector('.no-ten');
+    if (b.style.backgroundColor = 'skyblue') {
+        b.style.backgroundColor = 'lightgreen';
+        a.style.backgroundColor = 'skyblue';
+    } else {
+        b.style.backgroundColor = 'skyblue';
+        a.style.backgroundColor = 'lightgreen';
+    }
+}
+
+
+function clickResult() {
+    oneNo = document.querySelector('.no-one');
+    twoOk = document.querySelector('.ok-two');
+    threeNo = document.querySelector('.no-three');
+    fourOk = document.querySelector('.ok-four');
+    fiveNo = document.querySelector('.no-five');
+    sixOk = document.querySelector('.ok-six');
+    sevenOk = document.querySelector('.ok-seven');
+    eightOk = document.querySelector('.ok-eight');
+    nineOk = document.querySelector('.ok-nine');
+    tenNo = document.querySelector('.no-ten');
+    ok = document.querySelector('.question-ok');
+    no = document.querySelector('.question-no');
+
+    if (oneNo.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (oneNo.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (twoOk.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (twoOk.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (threeNo.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (threeNo.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (fourOk.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (fourOk.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (fiveNo.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (fiveNo.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (sixOk.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (sixOk.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (sevenOk.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (sevenOk.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (eightOk.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (eightOk.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (nineOk.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (nineOk.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+
+    if (tenNo.style.backgroundColor == 'lightgreen') {
+        sum += 10;
+    } else if (tenNo.style.backgroundColor == 'skyblue') {
+        sum -= 10;
+    }
+
+    if (sum <= 0) {
+        sum = 0;
+    }
+}
+
+function resultOpen() {
+    resultContainer = document.querySelector('.result-container2');
+    resultContainer.style.display = 'block';
+    value = document.querySelector('.value');
+    value.innerText = sum;
+}
